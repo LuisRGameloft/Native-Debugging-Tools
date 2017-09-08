d2f97eec11ecdce709461bde548a66cbffa9096a
David Landeros <david.landeros@gameloft.com>
2017-09-08 10:09:01 -0700

    Add example app, add dummy app

---
+++ app/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\AndroidManifest.xml : \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\AndroidManifest.xml \
---
+++ app/bin/classes.dex.d
@@ -0,0 +1,8 @@
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes.dex : \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\BuildConfig.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\MainActivity.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\R$attr.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\R$drawable.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\R$layout.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\R$string.class \
+C:\Users\david.landeros\Documents\Android-projects\Native-Debugging-Tools\app\bin\classes\com\android\ndt\R.class \
