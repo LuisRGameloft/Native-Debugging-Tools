108162253fc35b77c7669c62180b3801c3199121
Luis Rodas <luis.rodas@gameloft.com>
2018-12-24 09:38:39 -0800

    update[run debugger script] call debugger tool inside \NativeDebuggingTools path

--- run_debugger.bat
+++ run_debugger.bat
@@ -1,10 +1,14 @@
 @echo off
+set CUR_PATH=%cd%
 
-rem if exist config.bat
+rem Check if exist config.bat file
 if not exist "config.bat" (
 	echo You need to create config.bat using config_template file
 	goto end
 )
-
+call %CUR_PATH%\config.bat
+pushd %CUR_PATH%\NativeDebuggingTools
+call run_gdb_debugger.bat
+popd
 
 :end
