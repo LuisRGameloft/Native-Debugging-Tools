8b5eab41267e12c4d8c518713ce4bd95baa78d86
Luis Rodas <luis.rodas@gameloft.com>
2018-12-24 14:20:51 -0800

    feature(AndroidRemoteExec.cpp) Use Android Remove exe as only header

--- AndroidRemoteExec/AndroidRemoteExec.hpp
+++ AndroidRemoteExec/AndroidRemoteExec.hpp
@@ -21,15 +21,135 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+ 
+#ifndef ANDROID_REMOVE_EXEC_H_
+#define ANDROID_REMOVE_EXEC_H_
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <string>
-class AndroidRemoteExec
-{
-    public:
-        static void Init();
-    private:
-        static int s_iPort;
-        static char s_sPid[10];
-        static void* StartService(void*args);
-        static std::string& PreprocessCommand(std::string&);
-        static void Exec(const char *cmd);
-};
+#include <android/log.h> 
+
+#define REMOTE_SHELL_LOG(...) __android_log_print(ANDROID_LOG_INFO, "AndroidRemoteExec", __VA_ARGS__)
+
+namespace AndroidRemoteExec {
+
+	int  s_iPort = 3435;
+	char s_sPid[10] = "";
+
+	void Exec(const char *cmd)
+	{
+#if __ANDROID__			
+		if (fork() == 0)
+		{
+			FILE *fp;
+			char path[1035];
+			fp = popen(cmd, "r");
+			if (fp == NULL) {
+				REMOTE_SHELL_LOG("Failed to run command\n" );
+			}
+			while (fgets(path, sizeof(path)-1, fp) != NULL) {
+				REMOTE_SHELL_LOG("%s", path);
+			}
+			pclose(fp);
+			exit(0); // exit child process
+		}
+#endif		
+	}
+	
+	std::string& PreprocessCommand(std::string& cmd)
+	{
+#if __ANDROID__		
+		size_t pos = cmd.find("{PID}");
+		if(pos != std::string::npos)
+		{
+			cmd.replace(pos, 5, AndroidRemoteExec::s_sPid);
+		}
+		REMOTE_SHELL_LOG(">>> %s",cmd.c_str());
+		return cmd.append(" 2>&1");
+#else
+		return (std::string(""));
+#endif		
+	}
+	
+	void *StartService(void* args)
+	{
+#if __ANDROID__		
+		int client_fd = 0;
+		int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+		sockaddr_in listener;
+		listener.sin_family = AF_INET;
+		listener.sin_addr.s_addr = INADDR_ANY;
+		listener.sin_port = htons(AndroidRemoteExec::s_iPort);
+		bind(server_fd, (sockaddr*)&listener, sizeof listener);
+		listen(server_fd, SOMAXCONN);
+		while(true)
+		{
+			fd_set readset;
+			FD_ZERO(&readset);
+			FD_SET(server_fd, &readset);
+			if(client_fd != 0)
+			{
+				FD_SET(client_fd, &readset);
+			}
+			int no_events = select(FD_SETSIZE, &readset, NULL, NULL, NULL);
+			if(FD_ISSET(server_fd, &readset))
+			{
+				REMOTE_SHELL_LOG("New client connected\n" );
+				client_fd = accept(server_fd, NULL, NULL);
+				send(client_fd, "welcome to in-app-remote-shell\r\n", 32, 0);
+			}
+			else if(FD_ISSET(client_fd, &readset))
+			{
+				char buffer[1024];
+				memset(buffer, 0, 1024);
+				int len = recv(client_fd, buffer, sizeof buffer-1, 0);
+				if(len < 1) {
+					REMOTE_SHELL_LOG("Client disconnected\n" );
+					client_fd = 0;
+					continue;
+				}
+				buffer[len] = 0x00;
+				std::string cmd(buffer);
+				AndroidRemoteExec::Exec(AndroidRemoteExec::PreprocessCommand(cmd).c_str());
+				send(client_fd, "done\r\n", 6, 0);
+			}
+		}
+#endif		
+	}
+	
+	void Init()
+	{
+#if __ANDROID__
+		int pid = getpid();
+		sprintf(AndroidRemoteExec::s_sPid, "%i", pid);
+		FILE *pFile = fopen("/sdcard/commands.txt", "r");
+		if(pFile)
+		{
+			char c_str_cmd[500];
+			int r = 1;
+			while(r != 0)
+			{
+				r = fscanf(pFile, "%99[^\n]", c_str_cmd);
+				if(r != 0)
+				{
+					std::string cmd(c_str_cmd);
+					AndroidRemoteExec::Exec(AndroidRemoteExec::PreprocessCommand(cmd).c_str());
+				}
+			}
+			fclose(pFile);
+		}
+		pthread_t thread;
+		pthread_create(&thread, NULL, AndroidRemoteExec::StartService, NULL);
+#endif		
+	}
+}	
+
+#endif //ANDROID_REMOVE_EXEC_H_
